27a07bfd63e11bd15e6b83cd7795ab12b5a1c0b5
Matheesha Heshan <matheeshah02@gmail.com>
2026-08-03 13:17:05 +0530

    service

---
+++ broker-service/src/main/java/com/lanka/broker/service/BrokerService.java
@@ -0,0 +1,12 @@
+package com.lanka.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BrokerServiceApplication.class, args);
+    }
+}
+
