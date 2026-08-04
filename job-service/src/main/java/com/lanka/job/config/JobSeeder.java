6817c74dd604d15f254ed3813f8c9133f2a250f4
Matheesha Heshan <matheeshah02@gmail.com>
2026-08-04 20:17:59 +0530

    job service config

---
+++ job-service/src/main/java/com/lanka/job/config/JobSeeder.java
@@ -0,0 +1,9 @@
+package com.lanka.job.config;
+
+import com.lanka.job.dto.JobRequest;
+import com.lanka.job.repository.JobRepository;
+import com.lanka.job.service.JobService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
