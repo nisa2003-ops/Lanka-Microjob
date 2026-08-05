304940abdb94c3ef1c394f61da792df68ec45222
Matheesha Heshan <matheeshah02@gmail.com>
2026-08-05 20:49:24 +0530

    job service controller 2

--- job-service/src/main/java/com/lanka/job/controller/JobController.java
+++ job-service/src/main/java/com/lanka/job/controller/JobController.java
@@ -1,1 +1,7 @@
 package com.lanka.job.controller;
+import com.lanka.job.dto.JobRequest;
+import com.lanka.job.dto.JobResponse;
+import com.lanka.job.service.JobService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
