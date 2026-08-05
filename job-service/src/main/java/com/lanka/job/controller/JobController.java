65168d531dfd5a98d15ac76f4c0de01f1322d4e2
Matheesha Heshan <matheeshah02@gmail.com>
2026-08-05 20:50:19 +0530

    job service controller 3

--- job-service/src/main/java/com/lanka/job/controller/JobController.java
+++ job-service/src/main/java/com/lanka/job/controller/JobController.java
@@ -5,3 +5,12 @@ import com.lanka.job.service.JobService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+    private final JobService service;
+
+    public JobController(JobService service) {
+        this.service = service;
+    }
