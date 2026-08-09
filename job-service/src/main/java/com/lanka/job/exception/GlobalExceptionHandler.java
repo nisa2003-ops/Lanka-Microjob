3437269ff155bd0cf861768f4bb8ae8d01b79176
Matheesha Heshan <matheeshah02@gmail.com>
2026-08-09 10:08:33 +0530

    job service exception 2

--- job-service/src/main/java/com/lanka/job/exception/GlobalExceptionHandler.java
+++ job-service/src/main/java/com/lanka/job/exception/GlobalExceptionHandler.java
@@ -1,1 +1,5 @@
 package com.lanka.job.exception;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
