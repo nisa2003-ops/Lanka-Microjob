2cefc07663d1a2f149934164329423a4bef243f5
Matheesha Heshan <matheeshah02@gmail.com>
2026-08-23 11:46:11 +0530

    job service repo class

--- job-service/src/main/java/com/lanka/job/repository/JobRepository.java
+++ job-service/src/main/java/com/lanka/job/repository/JobRepository.java
@@ -4,3 +4,6 @@ import com.lanka.job.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+
+public interface JobRepository extends JpaRepository<Job, Long> {
+}
