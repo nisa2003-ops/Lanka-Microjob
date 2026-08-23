e0cf3443e18af28d5a433acb7789032c09d5c647
Matheesha Heshan <matheeshah02@gmail.com>
2026-08-23 11:46:57 +0530

    job service repo Distric

--- job-service/src/main/java/com/lanka/job/repository/JobRepository.java
+++ job-service/src/main/java/com/lanka/job/repository/JobRepository.java
@@ -6,4 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
+    List<Job> findByDistrict(String district);
+
+    List<Job> findByDistrictAndStatus(String district, String status);
+
+    List<Job> findByCityAndStatus(String city, String status);
+
+    List<Job> findByStatus(String status);
+
 }
