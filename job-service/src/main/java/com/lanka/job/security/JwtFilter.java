9bd5cd3ee8598d74d0ef8f0d5ff142be955ed08d
Matheesha Heshan <matheeshah02@gmail.com>
2026-08-24 00:51:46 +0530

    security of jwtFilter import all

--- job-service/src/main/java/com/lanka/job/security/JwtFilter.java
+++ job-service/src/main/java/com/lanka/job/security/JwtFilter.java
@@ -1,1 +1,12 @@
 package com.lanka.job.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
