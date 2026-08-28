0dc2a09c4df33004485d92c2c97dda4d23ba8e3a
Matheesha Heshan <matheeshah02@gmail.com>
2026-08-28 20:05:45 +0530

    security jwtutil key

--- job-service/src/main/java/com/lanka/job/security/JwtUtil.java
+++ job-service/src/main/java/com/lanka/job/security/JwtUtil.java
@@ -12,5 +12,10 @@ import java.security.Key;
 
 @Component
 public class JwtUtil {
+    private final Key key;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
 
 }
