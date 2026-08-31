c1b7e15a44c0bda953aa60629fabe99ac2517d6b
Matheesha Heshan <matheeshah02@gmail.com>
2026-08-31 11:21:29 +0530

    security SecurityConfig class

--- job-service/src/main/java/com/lanka/job/security/SecurityConfig.java
+++ job-service/src/main/java/com/lanka/job/security/SecurityConfig.java
@@ -12,3 +12,8 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+
+}
